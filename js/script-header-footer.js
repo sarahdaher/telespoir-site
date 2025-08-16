dba2694b0283c3bebe023cf37ad9cb9870fa0773
Sarah DAHER <143527452+sarahdaher@users.noreply.github.com>
2025-08-16 12:21:46 +0200

    Delete js directory

--- js/script-header-footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch('header.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('header').innerHTML = data);
-
-  fetch('footer.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('footer').innerHTML = data);
-});
